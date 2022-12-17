1022c4b417d151675c0807fbe605d3d7eca2036c
Peter Zingg <peter.zingg@gmail.com>
2022-12-16 17:38:51 -0800

    remove try block

--- lib/pleroma/workers/prune_feed_subscriptions_worker.ex
+++ lib/pleroma/workers/prune_feed_subscriptions_worker.ex
@@ -10,43 +10,37 @@ defmodule Pleroma.Workers.PruneFeedSubscriptionsWorker do
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: args}) do
-    try do
-      expiring =
-        case Map.get(args, "expiring") do
-          expiring when is_binary(expiring) ->
-            {:ok, expiring, _} = DateTime.from_iso8601(expiring)
-            DateTime.to_naive(expiring)
+    expiring =
+      case Map.get(args, "expiring") do
+        expiring when is_binary(expiring) ->
+          {:ok, expiring, _} = DateTime.from_iso8601(expiring)
+          DateTime.to_naive(expiring)
 
-          _ ->
-            NaiveDateTime.utc_now()
-        end
+        _ ->
+          NaiveDateTime.utc_now()
+      end
 
-      Logger.error("Pruning subscriptions and topics expiring before #{expiring}")
+    Logger.error("Pruning subscriptions and topics expiring before #{expiring}")
 
-      _ =
-        Subscriptions.list_inactive_subscriptions(expiring)
-        |> Enum.map(fn %Subscription{id: id} = subscription ->
-          _ = Subscriptions.final_unsubscribe(subscription)
-          id
-        end)
+    _ =
+      Subscriptions.list_inactive_subscriptions(expiring)
+      |> Enum.map(fn %Subscription{id: id} = subscription ->
+        _ = Subscriptions.final_unsubscribe(subscription)
+        id
+      end)
 
-      _ =
-        case Subscriptions.delete_all_inactive_subscriptions(@topic_lease_seconds, expiring) do
-          {:error, reason} ->
-            Logger.error("Transaction error deleting inactive subscriptions: #{reason}")
+    _ =
+      case Subscriptions.delete_all_inactive_subscriptions(@topic_lease_seconds, expiring) do
+        {:error, reason} ->
+          Logger.error("Transaction error deleting inactive subscriptions: #{reason}")
 
-          {count, _, _} ->
-            Logger.error("Deleted #{count} inactive subscriptions")
-        end
+        {count, _, _} ->
+          Logger.error("Deleted #{count} inactive subscriptions")
+      end
 
-      {count, _} = Subscriptions.delete_all_inactive_topics(expiring)
-      Logger.error("Deleted #{count} inactive topics")
+    {count, _} = Subscriptions.delete_all_inactive_topics(expiring)
+    Logger.error("Deleted #{count} inactive topics")
 
-      :ok
-    rescue
-      e ->
-        Logger.error("prune exception: #{inspect(e)}")
-        {:error, :went_bad}
-    end
+    :ok
   end
 end
